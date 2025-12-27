2ef4e1001973e0ce9721f8530f424378afd782a6
Nikhil98765 <42944335+Nikhil98765@users.noreply.github.com>
2025-12-28 00:17:41 +0530

    setting up redux-practice project

---
+++ redux-practice/src/store/index.js
@@ -0,0 +1,18 @@
+// import { createStore } from 'redux';
+
+// const reducer = (state, action) => {
+//   if (action.type === 'INCREMENT') {
+//     return {
+//       counter : state.counter + 1
+//     }
+//   }
+//   if (action.type === "DECREMENT") {
+//     return {
+//       counter: state.counter - 1,
+//     };
+//   }
+
+//   return state;
+// }
+
+// export const store = createStore(reducer);
