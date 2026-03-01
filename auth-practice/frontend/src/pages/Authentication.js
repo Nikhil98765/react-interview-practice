39a657b0f0291c29ee2f0c7430f162f347d3c5e0
Nikhil98765 <42944335+Nikhil98765@users.noreply.github.com>
2026-03-01 20:44:06 +0530

    Added action and display error messages for Authentication page

--- auth-practice/frontend/src/pages/Authentication.js
+++ auth-practice/frontend/src/pages/Authentication.js
@@ -1,7 +1,37 @@
+import { json, redirect } from 'react-router-dom';
 import AuthForm from '../components/AuthForm';
 
 function AuthenticationPage() {
   return <AuthForm />;
+}
+
+export const action = async ({ request }) => {
+  const searchParams = new URL(request.url).searchParams;
+  const mode = searchParams.get('mode');
+  
+  const formData = await request.formData();
+  const authObj = {
+    email: formData.get('email'),
+    password: formData.get('password')
+  };
+
+  const response = await fetch(`http://localhost:8080/${mode}`, {
+    method: 'POST',
+    body: JSON.stringify(authObj),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  if (response.status === 422 || response.status === 401) {
+    return response;
+  }
+
+  if (!response.ok) {
+    throw json({message: 'Could not authenticate user.'}, {status: 500})  
+  }
+
+  return redirect('/');
 }
 
 export default AuthenticationPage;
