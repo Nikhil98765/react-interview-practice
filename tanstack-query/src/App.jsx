7caf84d34cd031cef0b1c253519d69a98ceac5e0
Nikhil98765 <42944335+Nikhil98765@users.noreply.github.com>
2026-03-17 00:43:47 +0530

    feat(tanstack-query): integrate Tanstack Query
    - Add `@tanstack/react-query` dependency
    - Integrate `QueryClientProvider` in `App.jsx`
    - Refactor `NewEventsSection` with `useQuery`
    - Extract `fetchEvents` to `http.js` utility

--- tanstack-query/src/App.jsx
+++ tanstack-query/src/App.jsx
@@ -3,6 +3,8 @@ import {
   RouterProvider,
   createBrowserRouter,
 } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 
 import Events from './components/Events/Events.jsx';
 import EventDetails from './components/Events/EventDetails.jsx';
@@ -37,8 +39,14 @@ const router = createBrowserRouter([
   },
 ]);
 
+const queryClient = new QueryClient();
+
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
