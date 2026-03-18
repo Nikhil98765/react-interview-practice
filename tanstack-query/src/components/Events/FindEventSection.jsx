af0021d4302ab171c6f3a31b7aaf2e9ab815b72c
Nikhil98765 <42944335+Nikhil98765@users.noreply.github.com>
2026-03-18 19:26:05 +0530

    feat(events): lazy load search results
    - Prevent query until search term is set
    - Update `isPending` to `isLoading` hook property
    - Initialize search term as undefined

--- tanstack-query/src/components/Events/FindEventSection.jsx
+++ tanstack-query/src/components/Events/FindEventSection.jsx
@@ -7,11 +7,12 @@ import EventItem from './EventItem';
 
 export default function FindEventSection() {
   const searchElement = useRef();
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState();
 
-  const { data, isPending, isError, error } = useQuery({
+  const { data, isLoading, isError, error } = useQuery({
     queryKey: ['events', {search: searchTerm}],
-    queryFn: (ctxObj) => fetchEvents({...ctxObj, searchTerm})
+    queryFn: (ctxObj) => fetchEvents({ ...ctxObj, searchTerm }),
+    enabled: searchTerm !== undefined
   })
 
   function handleSubmit(event) {
@@ -21,8 +22,8 @@ export default function FindEventSection() {
       
   let content = <p>Please enter a search term and to find events.</p>;
   
-  if (isPending) {
-    content = <LoadingIndicator />
+  if (isLoading) {
+    content = <LoadingIndicator />;
   }
 
   if (isError) {
