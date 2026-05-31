3d7d622fa04abe40bd36895228f92c2954d733c0
Nikhil98765 <42944335+Nikhil98765@users.noreply.github.com>
2026-05-31 15:05:08 +0530

    feat(react-compiler): add compiler integration
    - Install compiler related packages
    - Configure ESLint plugin for checks
    - Add Babel plugin to Vite config

--- real-world-scenarios-js/eslint.config.js
+++ real-world-scenarios-js/eslint.config.js
@@ -4,6 +4,7 @@ import pluginReact from 'eslint-plugin-react';
 import reactHooksPlugin from 'eslint-plugin-react-hooks';
 import { defineConfig } from 'eslint/config';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import reactCompiler from 'eslint-plugin-react-compiler';
 
 export default defineConfig([
   { ignores: ['dist', 'node_modules'] },
@@ -20,6 +21,7 @@ export default defineConfig([
     },
     plugins: {
       'react-hooks': reactHooksPlugin,
+      'react-compiler': reactCompiler,
     },
     rules: {
       ...reactHooksPlugin.configs.recommended.rules,
@@ -27,6 +29,7 @@ export default defineConfig([
       'react/jsx-uses-react': 'off',
       'react-hooks/rules-of-hooks': 'error',
       'react-hooks/exhaustive-deps': 'warn',
+      'react-compiler/react-compiler': 'error',
       'no-unused-vars': 'warn',
     },
     settings: {
--- real-world-scenarios-js/vite.config.js
+++ real-world-scenarios-js/vite.config.js
@@ -1,15 +1,22 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { runSize } from 'vite-plugin-size';
+import babelPlugin from '@rolldown/plugin-babel';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), runSize('dist')],
+  plugins: [
+    react(),
+    babelPlugin({
+      presets: [reactCompilerPreset()]
+    }),
+    runSize('dist')
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
